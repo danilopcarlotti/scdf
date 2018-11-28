24aa80b89d7df8718a7b07898807e76b5a7c901e
danilo <danilopcarlotti@gmail.com>
2018-11-28 09:44:26 -0200

    atualização de ferramentas

---
+++ tika_textos.py
@@ -0,0 +1,17 @@
+import sys
+from tika import parser
+
+class tika_textos():
+	def __init__(self):
+		pass
+
+	def process_file(self,filepath):
+		file_data = parser.from_file(filepath)
+		return file_data['content'].strip()
+
+def main(filepath):
+	t = tika_textos()
+	print(t.process_file(filepath))
+
+if __name__ == '__main__':
+	main(sys.argv[1])
